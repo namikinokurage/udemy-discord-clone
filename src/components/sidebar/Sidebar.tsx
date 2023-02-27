6f8d910331a5bb762eca807b1ece0882416d121e
namiki-no-kurage <m.hirayama0129+namiki@gmail.com>
2023-02-27 23:29:40 +0000

    feat: チャンネルリストの追加

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./Sidebar.scss";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import AddIcon from "@mui/icons-material/Add";
+import SidebarChannel from "./SidebarChannel";
 
 const Sidebar = () => {
   return (
@@ -29,6 +30,13 @@ const Sidebar = () => {
               <h4>テストチャンネル</h4>
             </div>
             <AddIcon className="sidebarAddIcon" />
+          </div>
+          <div className="sidebarChannelList">
+            <SidebarChannel />
+            <SidebarChannel />
+            <SidebarChannel />
+            <SidebarChannel />
+            <SidebarChannel />
           </div>
         </div>
       </div>
