a55540bc482de8a2379d519242d498a5160168ae
namiki-no-kurage <m.hirayama0129+namiki@gmail.com>
2023-02-28 23:02:35 +0000

    feat: ユーザー情報をログイン情報から取得した情報に変更

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -7,11 +7,14 @@ import MicIcon from "@mui/icons-material/Mic";
 import HeadphonesIcon from "@mui/icons-material/Headphones";
 import SettingsIcon from "@mui/icons-material/Settings";
 import { auth } from "../../firebase";
+import { useAppSelector } from "../../app/hooks";
 
 const Sidebar = () => {
   const logout = () => {
     auth.signOut();
   };
+
+  const user = useAppSelector((state) => state.user);
 
   return (
     <div className="sidebar">
@@ -53,10 +56,10 @@ const Sidebar = () => {
         {/* sidebarFooter */}
         <div className="sidebarFooter">
           <div className="sidebarAccount">
-            <img src="./logo192.png" alt="" onClick={logout} />
+            <img src={user?.photo} alt="" onClick={logout} />
             <div className="accountName">
-              <h4>namikinokurage</h4>
-              <span>#9999</span>
+              <h4>{user?.displayName}</h4>
+              <span>#{user?.uid.substring(0, 4)}</span>
             </div>
           </div>
           <div className="sidebarVoice">
