38467017fe5402244fe54089e31bf1e196e02a91
namiki-no-kurage <m.hirayama0129+namiki@gmail.com>
2023-02-28 00:22:16 +0000

    feat: チャット画面のヘッダーを追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
-import React from 'react';
-import './App.scss';
-import Sidebar from './components/sidebar/Sidebar';
+import React from "react";
+import "./App.scss";
+import Chat from "./components/chat/Chat";
+import Sidebar from "./components/sidebar/Sidebar";
 
 function App() {
   return (
     <div className="App">
-    {/* sidebar */}
-    <Sidebar />
+      {/* sidebar */}
+      <Sidebar />
 
-    {/* home */}
+      {/* chat */}
+      <Chat />
     </div>
   );
 }
---
+++ src/components/chat/ChatHeader.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import "./ChatHeader.scss";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+import PushPinIcon from "@mui/icons-material/PushPin";
+import PeopleAltIcon from "@mui/icons-material/PeopleAlt";
+import SearchIcon from "@mui/icons-material/Search";
+import SendIcon from "@mui/icons-material/Send";
+import HelpIcon from "@mui/icons-material/Help";
+
+function ChatHeader() {
+  return (
+    <div className="chatHeader">
+      <div className="chatHeaderLeft">
+        <h3>
+          <span className="chatHeaderHash">#</span>
+          Test
+        </h3>
+      </div>
+      <div className="chatHeaderRight">
+        <NotificationsIcon className="chatHeaderRightIcon" />
+        <PushPinIcon className="chatHeaderRightIcon" />
+        <PeopleAltIcon className="chatHeaderRightIcon" />
+        <div className="chatHeaderSearch">
+          <input type="text" placeholder="検索" />
+          <SearchIcon className="chatHeaderRightIcon" />
+        </div>
+        <SendIcon className="chatHeaderRightIcon" />
+        <HelpIcon className="chatHeaderRightIcon" />
+      </div>
+    </div>
+  );
+}
+
+export default ChatHeader;
